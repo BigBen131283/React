d37c11474d68cc1398607a0104081f4a8b111284
BigBen83 <benjamin.toubhans@orange.fr>
2024-02-03 12:03:56 +0100

    Modal window

---
+++ 6.Créer-des-composants-courants/src/lessons/1.Créer-une-fenêtre-modale-avec-un-portal/ModalButton.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import { createPortal } from "react-dom"
+import ModalContent from "./ModalContent"
+
+export default function ModalButton() {
+  const [showModal, setShowModal] = useState(false)
+
+  return (
+    <>
+        <button
+        onClick={() => setShowModal(true)}
+        className="block mx-auto bg-slate-200 text-slate-900 p-2 rounded">
+            Open the modal
+        </button>
+        {showModal && createPortal(<ModalContent closeModal = {() => setShowModal(false)}/>, document.body)}
+        {/* closeModal est une props, c'est une fonction qui permet de changer le state du parent */}
+        {/* createPortal permet de rajouter un composant quelque part */}
+    </>
+  )
+}
